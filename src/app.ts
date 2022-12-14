484e6f8da3fe3eb9a850199d62b880030b03d8be
Jona <jonathanelian64@gmail.com>
2022-12-13 19:45:54 -0700

    Init infinity train api

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import { Application, Context } from "oak";
+
+const app = new Application();
+const port = 3000;
+
+app.use((ctx: Context) => {
+  ctx.response.body = "Hola Infinity Train";
+});
+
+console.log(`Server running in http://localhost:${port} 🦍`);
+
+await app.listen({ port });
