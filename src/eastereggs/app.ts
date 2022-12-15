ccf9905a8796361d09bf3c04496bca3169a31f6f
Jona <jonathanelian64@gmail.com>
2022-12-14 22:30:07 -0700

    Add One-One easteregg

--- src/eastereggs/app.ts
+++ src/eastereggs/app.ts
@@ -8,6 +8,12 @@ easterRouter.get(urlEastereggs, (ctx) => {
   ctx.response.body = easterData
 })
 
+easterRouter.get(`${urlEastereggs}/one-one`, (ctx) => {
+  ctx.response.body = {
+    congratulations: "You discovered a eastearegg 🐢 watch One-One 3d model",
+    model: "https://sketchfab.com/3d-models/infinity-train-one-one-097fcb2a99524e389f99115d08d62f8f"
+  }
+})
 
 
 export default easterRouter
