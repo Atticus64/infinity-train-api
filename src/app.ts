4d6d8dc188f88c13bb18edfebbe517681c9e9b8e
Jona <jonathanelian64@gmail.com>
2022-12-14 22:21:32 -0700

    Add 404 page

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
-import { Application, Context } from "oak";
+import { Application, Router } from "oak";
 
 import { apiRouter } from '$/apiRouter.ts'
 
 const app = new Application();
 const port = 3000;
 
+const rootRouter = new Router();
+
+rootRouter.get("/", (ctx) => {
+  ctx.response.body = "Hola Infinity Train 🚂 🐢";
+})
+
+app.use(rootRouter.routes())
+
 app.use(apiRouter.routes())
 
-app.use((ctx: Context) => {
-  ctx.response.body = "Hola Infinity Train 🚂 🐢";
-});
+app.use((ctx) => {
+  ctx.response.body = "404 not found"
+  ctx.response.status = 404
+})
 
 console.log(`Server running in http://localhost:${port} 🦍`);
 
