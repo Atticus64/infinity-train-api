90cad794c5876a1f264f1ba333ac018ad62b4d59
gustavocadev <churaalaygustavo11@gmail.com>
2023-02-10 12:23:41 -0500

    feat: add static files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { Context, Hono } from "hono";
+import { Hono } from "hono";
 import { serve } from "std/http/server.ts";
-import { cors, logger } from "hono_middleware";
+import { cors, logger, serveStatic } from "hono_middleware";
 import apiRouter from "./routes/apiRouter.ts";
 
 const app = new Hono();
@@ -14,18 +14,13 @@ app.use("*", logger());
 // import routes
 app.route("/api", apiRouter);
 
-// root route
-app.get("/", (c: Context) => {
-  return c.text("Hello World!");
-});
+// serve static files
+app.use("/*", serveStatic({ root: "./public" }));
 
 // 404 route
-app.get("*", (c) => {
-  return c.json({
-    msg: "404 not found",
-    suggestion: "try go to /api",
-  }, 404);
-});
+app.notFound((c) =>
+  c.json({ msg: "404 not found", suggestion: "try go to /api" }, 404)
+);
 
 // start server
 if (import.meta.main) await serve(app.fetch);
